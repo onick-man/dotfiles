1eb50de544947cf1c4adb4c5a9131d21bb7370dc
onick-man <fumihiko.takahashi.222@gmail.com>
2015-05-31 17:28:12 +0900

    update spaces

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+
 if which zsh; then
   # oh my zsh のインストール
   wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-
   # .zshrc.custom の追加
   echo 'source ~/.zshrc.custom' >> ~/.zshrc
-
   # .zshrc.custom のコピー
   cp .zshrc.custom ~/
 else
