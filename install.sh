f7c6aead7523ea2ba0d9a4f5a041f57a6aad5333
onick-man <fumihiko.takahashi.222@gmail.com>
2015-06-01 00:20:41 +0900

    use my .zshrc which already set

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 if which zsh; then
   # oh my zsh のインストール
   wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+  # .zshrcのコピー
+  cp -f .zshrc ~/
   # .zshrc.custom の追加
   echo 'source ~/.zshrc.custom' >> ~/.zshrc
   # .zshrc.custom のコピー
